ba6d06362d40859c613702c638fde3245cb84cda
sgarr754 <sgarr229@gmail.com>
2025-03-30 02:36:20 -0600

    Created PaintingDetails page and added to App.jsx

--- ArtGallery/src/App.jsx
+++ ArtGallery/src/App.jsx
@@ -5,6 +5,7 @@ import Gallery from './components/Gallery'
 import Artist from './components/Artist'
 import Genre from './components/Genre'
 import Painting from './components/Painting'
+import PaintingDetails from './components/PaintingDetails'
 import {Routes, Route} from 'react-router'
 
 // We need to require the use of conditional rendering in order
@@ -21,6 +22,7 @@ function App() {
         <Route path="/artist" element={<Artist />}/>
         <Route path="/genre" element={<Genre />}/>
         <Route path="/painting" element={<Painting />}/>
+        <Route path="/paintingdetails" element={<PaintingDetails />}/>
       </Routes>
     </main>
   )
---
+++ ArtGallery/src/components/PaintingDetails.jsx
@@ -0,0 +1,44 @@
+// This will be the Circuit Details pop-up page
+import { useState } from "react"
+
+const PaintingDetails = () => {
+
+    return(
+        <main className="pt-35 h-screen">
+            <div className="h-full w-full px-4">
+                <div className="max-w-4xl mx-auto">
+                    <div className="flex justify-between items-center mb-4">
+                        <h3 className="text-center text-lg font-semibold">
+                            Painting Details
+                        </h3>
+                    </div>
+
+                    <div className="space-x-2"> 
+                        <div className="absolute top-0 right-0 flex flex-col space-y-2 p-2">
+                            <button className="bg-white px-3 py-1 text-black rounded-lg text-sm">Close</button>
+                            <button className="bg-white px-3 py-1 text-black rounded-lg text-sm">Filter</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex">
+                    <div className="w-1/2 border border-blac p-3">
+                        <img alt="Painting image" className="w-full h-full object-cover">
+                        </img>
+                    </div>
+
+                    <div className="w-1/2 pl-3">
+                        <h2 className="text-2xl font-bold mb-2">Painting Title</h2>
+                        <h3 className="text-xl mb-4">Artist Name</h3>
+                        <p>info</p>
+
+                        <h3 className="font-semibold mb-2">Dominant Colours</h3>
+                    </div>
+                </div>
+
+            </div>
+        </main>
+    )
+}
+
+export default PaintingDetails
