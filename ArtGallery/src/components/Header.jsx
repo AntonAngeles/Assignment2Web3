86bd58595162158ebaa94eb0761c41435fe297e3
AntonAngeles <77523922+AntonAngeles@users.noreply.github.com>
2025-04-04 14:13:32 -0600

    minor changes

--- ArtGallery/src/components/Header.jsx
+++ ArtGallery/src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
         <div>
             <header className="fixed top-0 left-0 w-full shadow-sm z-10 bg-blue-500 text-white">
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold tracking-tight mb-5">Art Dashboard Project Gallery View</h1>
+                    <h1 className="text-3xl font-bold tracking-tight mb-5">Art Dashboard Project</h1>
                     <a href='./Artist'>
                         <button className={`text-white mr-2 p-2 px-4 rounded-xl ${getButton('/Artist')}`}>Artists</button>
                     </a>
