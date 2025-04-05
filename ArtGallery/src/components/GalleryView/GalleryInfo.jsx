a648ba7d5e5bddb9bd298dff951e369827a02b90
AntonAngeles <77523922+AntonAngeles@users.noreply.github.com>
2025-04-05 01:43:27 -0600

    fixed links, still need to add full picture

--- ArtGallery/src/components/GalleryView/GalleryInfo.jsx
+++ ArtGallery/src/components/GalleryView/GalleryInfo.jsx
@@ -2,14 +2,6 @@ import { useEffect } from 'react'
 import Map from "./Map"
 
 const GalleryInfo = (props) => {
-    const getLink = (link) => {
-        if (link) {
-            return (link)
-        } else {
-            return ""
-        }
-    }
-
     if (props.data.galleryId === undefined) {
         return <p>Please click a Gallery</p>
     } else {
@@ -24,7 +16,7 @@ const GalleryInfo = (props) => {
                 <p className="" >{props.data.galleryAddress}</p>
                 <h2 className="font-bold pt-1" >Country</h2>
                 <p className="" >{props.data.galleryCountry}</p>
-                <p className="underline mb-5">{getLink(props.data.galleryWebSite)}</p>
+                <a className="underline mb-5" href={props.data.galleryWebSite}>{props.data.galleryWebSite}</a>
                 <Map 
                     lat = {props.data.latitude}
                     long = {props.data.longitude}
